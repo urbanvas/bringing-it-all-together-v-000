74fc18fe7e5431350cc053c94fe83ace9fce7e6a
Neil Seejoor <nvseejoor@gmail.com>
2018-03-31 00:18:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id = nil,name:,breed: )
-    @id = id
+  def initialize(name:,breed: )
+    # @id = id
     @name = name
     @breed = breed
   end
